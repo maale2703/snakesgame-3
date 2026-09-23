f3cc2512b76f624f508adc5263bcfecad9e08079
maryana vitoria <160244232+maale2703@users.noreply.github.com>
2026-09-23 12:18:20 +0000

    versao 1

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
+import javax.swing.*;
 
+public class main { //teste
+    public static void main(string[] args) {
+        swingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame
+        })
+    }
+
+}
